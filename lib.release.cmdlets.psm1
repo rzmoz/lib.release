565ef79500f44082532435beb66602b98977e9f2
rzmoz <rzmoz@users.noreply.github.com>
2017-07-26 13:48:00 +0200

    Updated to support dotnet.core and vs 2017. Dropped support for older versions

--- lib.release.cmdlets.psm1
+++ lib.release.cmdlets.psm1
@@ -5,23 +5,19 @@ Function New-Lib.Release
         [string]$solutionName,
         [switch]$nonugets = $false,        
         [switch]$notests = $false,
-        [string]$testAssembliesFilter = "*.tests.dll",
-        [string]$msbuildConfiguration = "release"
+        [string]$testProjectFilter = "*.tests.csproj",
+        [string]$buildConfiguration = "release"
     )
 
     ##*********** Init ***********##
 
+	#trim solution name
+	$solutionName = $solutionName.Trim('.','\')
+
     $currentDir =  (Get-Item -Path ".\" -Verbose).FullName
     Write-Host "dir.current: $currentDir" -ForegroundColor Cyan
 
-    $buildScriptDir = Get-ProjectDir "lib.release"
-    Write-Host "lib.release.dir: $buildScriptDir" -ForegroundColor Cyan       
-    Write-Host "msbuild.configuration: $msbuildConfiguration" -ForegroundColor Cyan
-    Write-Host "nugets.enabled: $nugets" -ForegroundColor Cyan
-    $outputDir = "$slnDir\Output"
-    Write-Host "nugets.output.dir: $outputDir" -ForegroundColor Cyan
-    
-    Write-Host "##*********** sln ***********##" -ForegroundColor Cyan
+	Write-Host "##*********** sln ***********##" -ForegroundColor Cyan
 
     $slnDir = Get-ProjectDir $solutionName
     Write-Host "sln.dir: $slnDir" -ForegroundColor Cyan
@@ -32,13 +28,17 @@ Function New-Lib.Release
     $releaseParams = (Get-Content -Raw -Path $slnDir\lib.release.json | ConvertFrom-Json).projects
     Write-Host "sln.release.params:"$releaseParams -ForegroundColor Cyan    
 
-    Write-Host "##*********** test ***********##" -ForegroundColor Cyan
+    $buildScriptDir = Get-ProjectDir "lib.release"
+    Write-Host "lib.release.dir: $buildScriptDir" -ForegroundColor Cyan       
+    Write-Host "build.configuration: $buildConfiguration" -ForegroundColor Cyan
+    Write-Host "nugets.disabled: $nonugets" -ForegroundColor Cyan
+    $outputDir = "$slnDir\Output"
+    Write-Host "nugets.output.dir: $outputDir" -ForegroundColor Cyan    
 
-    Write-Host "test.disabled: $notests" -ForegroundColor Cyan
-    Write-Host "test.assemblies.filter: $testAssembliesFilter" -ForegroundColor Cyan    
-       
-    $consoleRunnerPath = Get-ChildItem -Path "$slnDir\packages" -Filter xunit.console.exe -Recurse -ErrorAction SilentlyContinue -Force| Sort-Object FullName | Select-Object -Last 1
-    Write-Host "test.runner.console.path:"$consoleRunnerPath.FullName -ForegroundColor Cyan    
+    Write-Host "##*********** tests ***********##" -ForegroundColor Cyan
+
+    Write-Host "tests.disabled: $notests" -ForegroundColor Cyan
+    Write-Host "tests.filter: $testProjectFilter" -ForegroundColor Cyan    
     
     
     if(Test-Path $slnDir ){        
@@ -74,71 +74,80 @@ Function New-Lib.Release
 
     Write-Host ($projects | Out-String) -ForegroundColor DarkGray
 
-    $assemblyInfos = $projects | % { $_."project.assemblyInfo" }
-    
+	#verify all changes are committed before proceeding	
+	if(Get-GitStatus $slnDir){
+		Write-Host "Git status is clean. good to go"
+	} else {
+		Write-Host "Git status not ready to release. Are all changes committed?"  -ForegroundColor Red
+		return -1
+	}
+	
+	$projectFiles = $projects | % { $_."project.file" }
+
+	##*********** Generate lib release package(s) ***********##
     try
-    {                        
-        ##*********** Build ***********##
-  
-        #clean repo for release - this will mess things up if everything is not committed!!
+	{
+	
+        #clean repo for release - this will mess things up if everything is not committed!		
         #https://git-scm.com/docs/git-clean
-        Write-Host "Cleaning repo for $msbuildConfiguration build"
+        Write-Host "Cleaning repo for $buildConfiguration build"
         Reset-GitDir $slnDir | Write-Host -ForegroundColor DarkGray
-        
-        #patch assembly infos
-        foreach($project in $projects){            
-            Update-AssemblyInfoVersions $project."project.assemblyInfo" $project."project.semVer10" $project."project.semVer20"
-        } 
-                
-        #restore nugets
-        #https://docs.nuget.org/consume/command-line-reference
-        Write-Host "Restoring nuget packages"
-        & "$buildScriptDir\nuget.exe" restore $slnPath | Write-Host -ForegroundColor DarkGray
-        
+  
+        #restore projects
+		$projectFiles | % { Restore-Project $_ }
+		
+		#patch project version
+        $projects| %{ Update-ProjectVersion $_."project.file" $_."project.semVer10" $_."project.semVer20"}
+				
         #build sln
         Write-Host "Building $slnPath"
-        & "C:\Program Files (x86)\MSBuild\14.0\Bin\amd64\MSBuild.exe" $slnPath /t:rebuild /p:Configuration=$msbuildConfiguration /verbosity:minimal | Write-Host -ForegroundColor DarkGray
-        
+        dotnet build $slnPath --configuration $buildConfiguration --no-incremental --verbosity minimal | Write-Host -ForegroundColor DarkGray
+        		
         #clean output dir if exists
         if(Test-Path $outputDir) { Remove-Item "$outputDir\*" -Force | Write-Host -ForegroundColor DarkGray }
         #create aritfacts dir
         New-Item $outputDir -ItemType Directory -Force | Write-Host -ForegroundColor DarkGray
         
-        $testsPassed = $notests;
+		$testsPassed = $true;
         
         if(-NOT ($notests)){            
-            if($consoleRunnerPath -eq $null) {
-                Write-Host "Unit test console runner not found! Unit tests will NOT be run" -ForegroundColor Red
-                $testsPassed=$false;
-            }
-            else {
-                #run unit tests
-                $testAssemblies = Get-ChildItem -Path "$slnDir" -Filter "$testAssembliesFilter" -Recurse | Where-Object { $_.FullName -like "*`\bin`\$msbuildConfiguration`\$testAssembliesFilter" -and $_.Attributes -ne "Directory" }
+				
+				$testProjects = Get-ChildItem -Path "$slnDir" -Filter "$testProjectFilter" -Recurse | Select-Object 
+				
+				Write-Host "Test projects found: $testProjects" -ForegroundColor DarkGray
 
-                & $consoleRunnerPath.FullName "C:\Projects\DotNet.Basics\DotNet.Basics.Tests\bin\Release\DotNet.Basics.Tests.dll"
+				$testProjects | % {					
+						$testResult = dotnet test $_.Directory.FullName --configuration $buildConfiguration --no-build
+						
+						Write-Host $testResult -ForegroundColor DarkGray
 
-                $testsPassed = ($lastexitcode -eq 0)
+						if(-NOT ($testResult -imatch 'Test Run Successful.')){
+							Write-Host "$_.Name tests failed" -ForegroundColor Red
+							$testsPassed = $false
+						} else {
+							Write-Host "$_.Name tests passed" -ForegroundColor DarkGray
+						}
+				}
 
                 if($testsPassed){
                     Write-Host "All tests passed" -ForegroundColor Green
-                } else {
-                    Write-Host "One ore more tests failed!" -ForegroundColor Red
-                }
-            }
-        }        
-        
+                }            
+        } 
+		        
         #create nugets if all tests passed
         if($testsPassed) {
-            #create nugets and place in output Dir dir
-            foreach($project in $projects) {
-                $nugetTarget = $project."nuget.target"
-                $nugetVersion = $project."project.semVer10"
-                #https://docs.nuget.org/consume/command-line-reference
-                Write-Host "Packing $nugetTarget -v $nugetVersion"
-                & "$buildScriptDir\nuget.exe" pack $nugetTarget -Properties "Configuration=$msbuildConfiguration;Platform=AnyCPU" -version $nugetVersion  -OutputDirectory $outputDir  | Write-Host -ForegroundColor DarkGray
-            }            
-
+            
             if(-NOT ($nonugets)) {
+
+				#create nugets and place in output Dir dir
+				foreach($project in $projects) {
+	                $projectFile = $project."project.file"
+					$packageVersion = $project."project.semVer10"
+                
+					Write-Host "Packing $projectFile -v $packageVersion"
+					dotnet pack $projectFile --configuration $buildConfiguration --no-build --output $outputDir  | Write-Host -ForegroundColor DarkGray
+				}
+
                 $apiKey = Read-Host "Please enter nuget API key"
                 #https://docs.nuget.org/consume/command-line-reference
                 Get-ChildItem $outputDir -Filter "*.nupkg" | % { 
@@ -150,18 +159,19 @@ Function New-Lib.Release
         }
         else {
             Write-host "Build failed!" -ForegroundColor Red
-        }        
+        }
+		
     } finally {        
         #clean output Dir if exists
         if(Test-Path $outputDir) { Remove-Item "$outputDir\*" -Force | Write-Host -ForegroundColor DarkGray }
+		if(Test-Path $outputDir) { Remove-Item "$outputDir" -Force | Write-Host -ForegroundColor DarkGray }
 
-        
-        #revert assembly info
-        $assemblyInfos | Undo-AssemblyInfoVersions
-
+		#revert project version
+        $projectFiles | Undo-ProjectVersion
+		
         Write-Host "Setting location $currentDir" | Write-Host -ForegroundColor DarkGray
         sl $currentDir        
-    }
+    }	
 }
 
 Function New-ProjectInfo
@@ -179,6 +189,9 @@ Function New-ProjectInfo
     $projectName = $solutionName+$projectParams.extension
     $projectInfo.Add("project.name",$projectName);
 
+	$projectFile = "$slnDir\$projectName\$projectName.csproj"
+    $projectInfo.Add("project.file",$projectFile);
+
     $projectDir = "$slnDir\$projectName"
     $projectInfo.Add("project.dir",$projectDir);
 
@@ -190,73 +203,19 @@ Function New-ProjectInfo
             
     $semver20 = "$semver10+$commitsSinceInit.$commitHash"
     $projectInfo.Add("project.semVer20",$semver20);
-
-    $assemblyInfo = (Get-AssemblyInfos $projectDir).FullName
-    $projectInfo.Add("project.assemblyInfo",$assemblyInfo);
-
-    $nugetTarget = "$slnDir\$projectName\$projectName.csproj"
-    $projectInfo.Add("nuget.target",$nugetTarget);    
-
+	
     return $projectInfo    
 }
 
-Function Get-AssemblyInfos{
-    Param (
-        [Parameter(Mandatory=$true)]
-        [string]$Rootdir
-    )
-    
-    return Get-ChildItem $Rootdir -Filter "AssemblyInfo.cs" -recurse | Where-Object { $_.Attributes -ne "Directory"}
-}
-
-
-Function Update-AssemblyInfoVersions
+Function Restore-Project
 {
-    Param (
+Param (
         [Parameter(Mandatory=$true)]
-        [string]$assemblyInfoFullName,
-        [Parameter(Mandatory=$true)]
-        [string]$Version,
-        [Parameter(Mandatory=$true)]
-        [string]$SemVer20
+        [string]$ProjectPath
     )
-  
-    Write-Host "Updating AssemblyInfos"
 
-        $fullName=$assemblyInfoFullName
-        Write-host "Updating $fullName" -ForegroundColor DarkGray
-        $TmpFile = $assemblyInfoFullName + ".tmp"   
-        Write-host "Backup: $TmpFile"  -ForegroundColor DarkGray
-
-        #backup file for reverting later
-        Copy-Item $fullName $TmpFile
-
-        [regex]$patternAssemblyVersion = "(AssemblyVersion\("")(\d+\.\d+\.\d+\.\d+)(""\))"
-        $replacePatternAssemblyVersion = "`${1}$($Version)`$3"
-        [regex]$patternAssemblyFileVersion = "(AssemblyFileVersion\("")(\d+\.\d+\.\d+\.\d+)(""\))"
-        $replacePatternAssemblyFileVersion = "`${1}$($Version)`$3"
-        [regex]$patternAssemblyInformationalVersion = "(AssemblyInformationalVersion\("")(\d+\.\d+\.\d+\.\d+)(""\))"
-        $replacePatternAssemblyInformationalVersion = "`${1}$($SemVer20)`$3"
-
-        # run the regex replace        
-        $updated = Get-Content -Path $fullName |
-            % { $_ -replace $patternAssemblyVersion, $replacePatternAssemblyVersion } |
-            % { $_ -replace $patternAssemblyFileVersion, $replacePatternAssemblyFileVersion } |
-            % { $_ -replace $patternAssemblyInformationalVersion, $replacePatternAssemblyInformationalVersion }
-        Set-Content $fullName -Value $updated -Force
-    
-}
-Function Undo-AssemblyInfoVersions
-{
-    Write-host "Reverting assemblyInfos"
-    foreach ($o in $input)
-    {
-        $TmpFile = $o + ".tmp"
-        Write-host "Reverting $TmpFile" -ForegroundColor DarkGray
-        if(Test-Path($TmpFile)){            
-            Move-Item  $TmpFile $o -Force
-        }        
-    }
+	Write-Host "Restoring $ProjectPath"
+	dotnet restore $ProjectPath | Write-Host -ForegroundColor DarkGray
 }
 
 Function Get-ProjectDir
@@ -274,6 +233,33 @@ Function Get-ProjectDir
     else {
         return $projectDir
     }
+}
+
+Function Get-GitStatus
+{
+    Param (
+        [Parameter(Mandatory=$true)]
+        [string]$ProjectName
+    )
+
+    $projectDir = Get-ProjectDir $ProjectName
+
+    $currentDir = (Get-Location).Path
+
+    try{
+        sl $projectDir
+		Write-Host "Asserting Git status is ready for release" -ForegroundColor DarkGray
+        $gitStatus = git status | Out-String 
+		Write-Host $gitStatus -ForegroundColor DarkGray
+		
+		if($gitStatus -imatch 'nothing to commit, working tree clean'){
+			return $true
+		} else {			
+			return $false
+		}
+	} finally {
+        sl $currentDir
+    }   
 }
 
 Function Reset-GitDir
@@ -295,4 +281,69 @@ Function Reset-GitDir
     }
 
     Write-Verbose "Git dir: $projectDir"
+}
+
+Function Update-ProjectVersion
+{
+    Param (
+        [Parameter(Mandatory=$true)]
+        [string]$ProjectFullName,
+        [Parameter(Mandatory=$true)]
+        [string]$Version,
+        [Parameter(Mandatory=$true)]
+        [string]$SemVer20
+    )
+  
+    Write-Host "Updating project version for $ProjectFullName"
+
+    $fullName = $ProjectFullName
+    Write-host "Updating $fullName" -ForegroundColor DarkGray
+    $TmpFile = $ProjectFullName + ".tmp"   
+    Write-host "Backup: $TmpFile"  -ForegroundColor DarkGray
+
+    #backup file for reverting later
+    Copy-Item $fullName $TmpFile
+
+	#load project xml
+	[xml]$xml = Get-Content -Path $fullName 
+
+	#ensure version nodes exist
+	$propertyGroupNode = $xml.SelectSingleNode("//Project/PropertyGroup")
+	if ($propertyGroupNode -eq $null) {
+		Write-Host "csproj format not recognized. Is this a valid VS 17 project file?" |-ForegroundColor Red
+		return
+	}
+		
+	if ($propertyGroupNode.Version -eq $null) {
+		$propertyGroupNode.AppendChild($xml.CreateElement("Version")) | Write-Host -ForegroundColor DarkGray
+	}
+
+	if ($propertyGroupNode.AssemblyVersion -eq $null) {
+		$propertyGroupNode.AppendChild($xml.CreateElement("AssemblyVersion"))| Write-Host -ForegroundColor DarkGray
+	}
+	
+	if ($propertyGroupNode.FileVersion -eq $null) {
+		$propertyGroupNode.AppendChild($xml.CreateElement("FileVersion"))| Write-Host -ForegroundColor DarkGray
+	}
+
+	#update versions
+	$propertyGroupNode.SelectSingleNode("//Version").InnerText = $SemVer20
+	$propertyGroupNode.SelectSingleNode("//AssemblyVersion").InnerText = $Version
+	$propertyGroupNode.SelectSingleNode("//FileVersion").InnerText = $Version
+		
+	#write to project file
+    $xml.Save($fullName)	
+}
+
+Function Undo-ProjectVersion
+{
+    Write-host "Reverting project version"
+    foreach ($o in $input)
+    {
+        $TmpFile = $o + ".tmp"
+        Write-host "Reverting $TmpFile" -ForegroundColor DarkGray
+        if(Test-Path($TmpFile)){            
+            Move-Item $TmpFile $o -Force
+        }        
+    }
 }
