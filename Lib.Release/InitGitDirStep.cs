e996ac5c8ae2e5fd6fae13f36a1acdc98036819e
rzmoz <rzmoz@users.noreply.github.com>
2025-02-19 11:51:49 +0100

    cc pipeline steps ioc

---
+++ Lib.Release/InitGitDirStep.cs
@@ -0,0 +1,42 @@
+﻿using DotNet.Basics.Collections;
+using DotNet.Basics.IO;
+using DotNet.Basics.Pipelines;
+using DotNet.Basics.Serilog.Looging;
+using DotNet.Basics.Sys;
+using LibGit2Sharp;
+
+namespace Lib.Release
+{
+    public class InitGitDirStep(ILoog log) : PipelineStep<LibReleasePipelineArgs>
+    {
+        protected override Task<int> RunImpAsync(LibReleasePipelineArgs args)
+        {
+            using (var repo = new Repository(args.LibRootDir))
+            {
+                var status = repo.RetrieveStatus(new StatusOptions
+                {
+                    ExcludeSubmodules = true,
+                    IncludeIgnored = false
+                });
+
+                if (status.Any())//pending changes => no good to release
+                {
+                    log.Error($"There are {status.Count()} pending changes. Commit before release!");
+                    foreach (var item in status)
+                    {
+                        log.Debug($"{item.FilePath}:{item.State.ToName()}");
+                    }
+                    return Task.FromResult(400);
+                }
+
+                var binDirs = args.LibRootDir.ToDir().GetDirectories("bin", SearchOption.AllDirectories);
+
+
+                log.Debug("Cleaning bin dirs");
+                binDirs.ForEach(d => log.Verbose(d.FullName));
+                binDirs.ForEach(d => d.DeleteIfExists());
+            }
+            return Task.FromResult(0);
+        }
+    }
+}
--- Lib.Release/ReleasePipeline.cs
+++ Lib.Release/ReleasePipeline.cs
@@ -1,9 +1,7 @@
-﻿using DotNet.Basics.Collections;
-using DotNet.Basics.IO;
+﻿using DotNet.Basics.IO;
 using DotNet.Basics.Pipelines;
 using DotNet.Basics.Serilog.Looging;
 using DotNet.Basics.Sys;
-using LibGit2Sharp;
 
 namespace Lib.Release
 {
@@ -15,7 +13,7 @@ namespace Lib.Release
         {
             _log = log;
             AddStep(nameof(AssertLibRootDir), AssertLibRootDir);
-            AddStep(nameof(InitGitDirForRelease), InitGitDirForRelease);
+            AddStep<InitGitDirStep>();
         }
 
         public Task<int> AssertLibRootDir(LibReleasePipelineArgs args)
@@ -25,35 +23,6 @@ namespace Lib.Release
             if (!args.LibRootDir.ToDir().Exists())
                 throw new DirectoryNotFoundException(args.LibRootDir.ToDir().FullName);
 
-            return Task.FromResult(0);
-        }
-        public Task<int> InitGitDirForRelease(LibReleasePipelineArgs args)
-        {
-            using (var repo = new Repository(args.LibRootDir))
-            {
-                var status = repo.RetrieveStatus(new StatusOptions
-                {
-                    ExcludeSubmodules = true,
-                    IncludeIgnored = false
-                });
-
-                if (status.Any())//pending changes => no good to release
-                {
-                    _log.Error($"There are {status.Count()} pending changes. Commit before release!");
-                    foreach (var item in status)
-                    {
-                        _log.Debug($"{item.FilePath}:{item.State.ToName()}");
-                    }
-                    return Task.FromResult(400);
-                }
-
-                var binDirs = args.LibRootDir.ToDir().GetDirectories("bin", SearchOption.AllDirectories);
-
-
-                _log.Debug("Cleaning bin dirs");
-                binDirs.ForEach(d => _log.Verbose(d.FullName));
-                binDirs.ForEach(d => d.DeleteIfExists());
-            }
             return Task.FromResult(0);
         }
     }
